92f6aba6e14ee5f8d73ef4c992d65115893ed993
xiaosiqi <xiaosiqi@XIAOSIQI-PC>
2017-06-21 12:11:54 +0800

    添加项目文件。

---
+++ ClassLibrary1/myLibary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myLibary
+{
+    public class match
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+
+        }
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+
+        }
+        public static double Mul(double a, double b)
+        {
+            return a * b;
+
+        }
+        public static double Div(double a, double b)
+        {
+            if (b == 0)
+            { return double.PositiveInfinity; }
+            else
+            { return a / b; }
+
+
+        }
+    }
+}
---
+++ 命令行 hello world！/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using myLibary;
+
+namespace 命令行_hello_world_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double a = match.Add(11, 22);
+            Console.WriteLine(a);
+            double b = match.Div(11, 0);
+            Console.WriteLine(b);
+            double c = match.Mul(900,520);
+            Console.WriteLine(c);
+            Form cccc = new Form();
+            cccc.Text = "命令行窗口";
+            cccc.Width = 900;
+            cccc.Height = 900;
+            cccc.ShowDialog();
+        
+        }
+    }
+}
